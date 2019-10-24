cf47ff1b5a086c3375cb533de229fab79caf39a3
Chenxu <chenxu2018@outlook.com>
2019-10-24 22:16:22 +1100

    chat

---
+++ Chat/app/src/main/java/com/example/chat/ChatActivity.java
@@ -0,0 +1,66 @@
+package com.example.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private List<Msg> msgList = new ArrayList<>();
+
+    private EditText inputText;
+
+    private Button send;
+
+    private RecyclerView msgRecyclerView;
+
+    private MsgAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        //修改成名字
+        setTitle("???");
+        initMsgs();
+        inputText = (EditText) findViewById(R.id.input_text);
+        send = (Button) findViewById(R.id.send);
+        msgRecyclerView = (RecyclerView) findViewById(R.id.msg_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        msgRecyclerView.setLayoutManager(layoutManager);
+        adapter = new MsgAdapter(msgList);
+        msgRecyclerView.setAdapter(adapter);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String content = inputText.getText().toString();
+                if(!"".equals(content)) {
+                    Msg msg = new Msg(content, Msg.TYPE_SENT);
+                    msgList.add(msg);
+                    adapter.notifyItemInserted(msgList.size() - 1);
+                    msgRecyclerView.scrollToPosition(msgList.size() - 1);
+                    inputText.setText("");
+                }
+            }
+        });
+    }
+
+    private void initMsgs() {
+        Msg msg1 = new Msg("Hello", Msg.TYPE_RECEIVED);
+        msgList.add(msg1);
+        Msg msg2 = new Msg("ThankU", Msg.TYPE_SENT);
+        msgList.add(msg2);
+        Msg msg3 = new Msg("What's ur problem", Msg.TYPE_RECEIVED);
+        msgList.add(msg3);
+    }
+
+}
